42e42aa514d88ad6871996844de01b0f3f25c0c5
Sogen1118 <65271351+Sogen1118@users.noreply.github.com>
2020-05-20 16:51:08 +0900

    タワーManager

---
+++ AnimalForest/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,15 @@
+﻿//F.D.
+//タワーのデータ構造クラス
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    public int hp;
+    public bool number;
+    public int damage;
+    public int max_hp;
+    //int max_number = 5;
+}
