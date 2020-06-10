b352304c82d3cf806bd2018d3318d4c94ce0bede
Sogen1118 <65271351+Sogen1118@users.noreply.github.com>
2020-06-10 14:34:31 +0900

    ギミックのprefab化

--- AnimalForest/Assets/Scripts/Tower/TowerManager.cs
+++ AnimalForest/Assets/Scripts/Tower/TowerManager.cs
@@ -12,6 +12,42 @@ public class TowerManager : MonoBehaviour
     public bool number;
     public int damage;
     public int max_hp;
+    private GameObject nearObj;
+
 
     //int max_number = 5;
+
+    //一番近いタワーを探す::動くかは不明
+    //private void Update()
+    //{
+    //    nearObj = SerchTag(gameObject, "Tower");
+    //}
+
+    //private GameObject SerchTag(GameObject nowObj, string tagname)
+    //{
+    //    float templateDistance = 0;
+    //    float nearDistance = 0;
+    //    string nearObjName;
+    //    GameObject targetObj = null;
+
+    //    //タグ指定されたオブジェクトを配列で取得する
+    //    foreach (GameObject obs in GameObject.FindGameObjectsWithTag(tagname))
+    //    {
+    //        //自身と取得したオブジェクトの距離を取得
+    //        templateDistance = Vector3.Distance(obs.transform.position, nowObj.transform.position);
+
+    //        //オブジェクトの距離が近いか、距離0であればオブジェクト名を取得
+    //        //一時変数に距離を格納
+    //        if (nearDistance == 0 || nearDistance > templateDistance)
+    //        {
+    //            nearDistance = templateDistance;
+    //            //nearObjName = obs.name;
+    //            targetObj = obs;
+    //        }
+
+    //    }
+    //    //最も近かったオブジェクトを返す
+    //    //return GameObject.Find(nearObjName);
+    //    return targetObj;
+    //}
 }
