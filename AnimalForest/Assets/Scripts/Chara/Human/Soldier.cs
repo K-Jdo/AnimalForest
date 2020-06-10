710d4c1ac5b52e40ae8b7e3b9f389cfbbf897299
Meat_Ano <jo-kazu@outlook.jp>
2020-06-10 14:46:51 +0900

    なぜかもとに戻っていたので再度修正@城戸

--- AnimalForest/Assets/Scripts/Chara/Human/Soldier.cs
+++ AnimalForest/Assets/Scripts/Chara/Human/Soldier.cs
@@ -6,7 +6,7 @@ public class Soldier : Human
     protected override void Awake()
     {
         base.Awake();
-        status = new Status(250.0f, 20.0f, 0.0f, 20.0f, 50, "小人間");
+        status = new Status(250.0f, 20.0f, 0.0f, 2.3f, 50, "小人間");
         agent.speed = status.speed;
     }
 }
