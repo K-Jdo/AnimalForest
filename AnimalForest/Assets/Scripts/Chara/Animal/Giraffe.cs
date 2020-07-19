2a8f7cfad417918caca49e1cd6c663bc99c000c9
natsuiroFS <rin.n.s.1103@hotmail.co.jp>
2020-07-20 02:08:01 +0900

    giraffe.csの修正 ＠藤原

--- AnimalForest/Assets/Scripts/Chara/Animal/Giraffe.cs
+++ AnimalForest/Assets/Scripts/Chara/Animal/Giraffe.cs
@@ -12,20 +12,10 @@ public class Giraffe : Animal
     {
         status = new Status(150, 25, 10, 1.5f, 1000, "キリン");
         base.Awake();
-<<<<<<< HEAD
 
-        //my_range_attack = test_obj.GetComponent<RangeAttaker>();
-        //my_range_attack.my_type = character_type;
-        //range = my_range_attack.SearchRadius;
-        range = 1;
-
-        //my_range_attack = test_obj.GetComponent<RangeAttaker>();
-        //my_range_attack.my_type = character_type;
-=======
         GameObject obj = transform.GetChild(0).gameObject;
         my_renderer = obj.GetComponent<Renderer>();
         anim = obj.GetComponent<Animator>();
->>>>>>> 1d4e8590834d1426cdbec5d3b4a598678fb455ca
     }
 
     /// <summary>
