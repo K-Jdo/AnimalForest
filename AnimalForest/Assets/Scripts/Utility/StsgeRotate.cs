7825c96c3c55e9743308dc7c6d8aa9f6d6ab5793
Meat_Ano <jo-kazu@outlook.jp>
2020-08-26 00:55:55 +0900

    ステージの回転とブタのエフェクト修正、タイトル用にランダムで動き回るクラスを作成@城戸

---
+++ AnimalForest/Assets/Scripts/Utility/StsgeRotate.cs
@@ -0,0 +1,12 @@
+﻿// K.Joudo. 2020
+using UnityEngine;
+
+// ステージを回転させるクラス
+public class StsgeRotate : MonoBehaviour
+{
+    [SerializeField] float y = 0.05f;
+    void Update()
+    {
+        transform.Rotate(0.0f, y, 0.0f);
+    }
+}
